d284b4a6c39896db881968db4c02a2ee4a3f5927
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-06-01 00:12:52 -0300

    formato login

--- TP3/Blockbuster UI/Login.Designer.cs
+++ TP3/Blockbuster UI/Login.Designer.cs
@@ -28,14 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.txtClave = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.picBlockLogin = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlockLogin)).BeginInit();
             this.SuspendLayout();
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(29, 79);
+            this.txtUsuario.Location = new System.Drawing.Point(29, 124);
             this.txtUsuario.Name = "txtUsuario";
             this.txtUsuario.PlaceholderText = "Usuario";
             this.txtUsuario.Size = new System.Drawing.Size(245, 23);
@@ -43,7 +46,7 @@
             // 
             // txtClave
             // 
-            this.txtClave.Location = new System.Drawing.Point(29, 125);
+            this.txtClave.Location = new System.Drawing.Point(29, 169);
             this.txtClave.Name = "txtClave";
             this.txtClave.PasswordChar = '*';
             this.txtClave.PlaceholderText = "Clave";
@@ -52,7 +55,7 @@
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(29, 202);
+            this.btnLogin.Location = new System.Drawing.Point(29, 260);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(245, 38);
             this.btnLogin.TabIndex = 2;
@@ -60,11 +63,25 @@
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
+            // picBlockLogin
+            // 
+            this.picBlockLogin.BackColor = System.Drawing.Color.Transparent;
+            this.picBlockLogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBlockLogin.BackgroundImage")));
+            this.picBlockLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBlockLogin.Location = new System.Drawing.Point(44, 0);
+            this.picBlockLogin.Name = "picBlockLogin";
+            this.picBlockLogin.Size = new System.Drawing.Size(215, 118);
+            this.picBlockLogin.TabIndex = 3;
+            this.picBlockLogin.TabStop = false;
+            // 
             // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(303, 333);
+            this.Controls.Add(this.picBlockLogin);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtClave);
             this.Controls.Add(this.txtUsuario);
@@ -73,6 +90,7 @@
             this.Name = "login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBlockLogin)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,5 +101,6 @@
         private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.TextBox txtClave;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox picBlockLogin;
     }
 }
