b1c8a7a223308ee508500ff0fb9f76d25effc70f
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-06-10 15:43:51 -0300

    avences form modificacion

--- TP4/Blockbuster UI/AgregarEmpleado.Designer.cs
+++ TP4/Blockbuster UI/AgregarEmpleado.Designer.cs
@@ -30,18 +30,33 @@
         {
             this.txtNombreEmpleado = new System.Windows.Forms.TextBox();
             this.btnConfirmar = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtApellidoEmpleado = new System.Windows.Forms.TextBox();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.lblNombreUsuario = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chkEsAdmin = new System.Windows.Forms.CheckBox();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblSignoPesos = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.lblFechaNacimiento = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtNombreEmpleado
             // 
-            this.txtNombreEmpleado.Location = new System.Drawing.Point(61, 33);
+            this.txtNombreEmpleado.Location = new System.Drawing.Point(42, 45);
             this.txtNombreEmpleado.Name = "txtNombreEmpleado";
-            this.txtNombreEmpleado.Size = new System.Drawing.Size(342, 23);
+            this.txtNombreEmpleado.Size = new System.Drawing.Size(188, 23);
             this.txtNombreEmpleado.TabIndex = 0;
             // 
             // btnConfirmar
             // 
-            this.btnConfirmar.Location = new System.Drawing.Point(328, 92);
+            this.btnConfirmar.Location = new System.Drawing.Point(328, 347);
             this.btnConfirmar.Name = "btnConfirmar";
             this.btnConfirmar.Size = new System.Drawing.Size(75, 23);
             this.btnConfirmar.TabIndex = 1;
@@ -49,11 +64,151 @@
             this.btnConfirmar.UseVisualStyleBackColor = true;
             this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
             // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(42, 27);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(57, 15);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre: ";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(42, 71);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(54, 15);
+            this.lblApellido.TabIndex = 4;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // txtApellidoEmpleado
+            // 
+            this.txtApellidoEmpleado.Location = new System.Drawing.Point(42, 89);
+            this.txtApellidoEmpleado.Name = "txtApellidoEmpleado";
+            this.txtApellidoEmpleado.Size = new System.Drawing.Size(188, 23);
+            this.txtApellidoEmpleado.TabIndex = 3;
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(42, 115);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(30, 15);
+            this.lblDni.TabIndex = 6;
+            this.lblDni.Text = "DNI:";
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(42, 133);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(188, 23);
+            this.txtDni.TabIndex = 5;
+            // 
+            // lblNombreUsuario
+            // 
+            this.lblNombreUsuario.AutoSize = true;
+            this.lblNombreUsuario.Location = new System.Drawing.Point(42, 159);
+            this.lblNombreUsuario.Name = "lblNombreUsuario";
+            this.lblNombreUsuario.Size = new System.Drawing.Size(66, 15);
+            this.lblNombreUsuario.TabIndex = 8;
+            this.lblNombreUsuario.Text = "Username: ";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(42, 177);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(188, 23);
+            this.txtUsername.TabIndex = 7;
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.Location = new System.Drawing.Point(42, 203);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(39, 15);
+            this.lblClave.TabIndex = 10;
+            this.lblClave.Text = "Clave:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 221);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(188, 23);
+            this.textBox1.TabIndex = 9;
+            // 
+            // chkEsAdmin
+            // 
+            this.chkEsAdmin.AutoSize = true;
+            this.chkEsAdmin.Location = new System.Drawing.Point(42, 311);
+            this.chkEsAdmin.Name = "chkEsAdmin";
+            this.chkEsAdmin.Size = new System.Drawing.Size(102, 19);
+            this.chkEsAdmin.TabIndex = 11;
+            this.chkEsAdmin.Text = "Administrador";
+            this.chkEsAdmin.UseVisualStyleBackColor = true;
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Location = new System.Drawing.Point(93, 247);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(45, 15);
+            this.lblSalario.TabIndex = 15;
+            this.lblSalario.Text = "Salario:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 265);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(137, 23);
+            this.textBox2.TabIndex = 14;
+            // 
+            // lblSignoPesos
+            // 
+            this.lblSignoPesos.AutoSize = true;
+            this.lblSignoPesos.Location = new System.Drawing.Point(59, 268);
+            this.lblSignoPesos.Name = "lblSignoPesos";
+            this.lblSignoPesos.Size = new System.Drawing.Size(13, 15);
+            this.lblSignoPesos.TabIndex = 16;
+            this.lblSignoPesos.Text = "$";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(271, 51);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 18;
+            // 
+            // lblFechaNacimiento
+            // 
+            this.lblFechaNacimiento.AutoSize = true;
+            this.lblFechaNacimiento.Location = new System.Drawing.Point(271, 27);
+            this.lblFechaNacimiento.Name = "lblFechaNacimiento";
+            this.lblFechaNacimiento.Size = new System.Drawing.Size(104, 15);
+            this.lblFechaNacimiento.TabIndex = 19;
+            this.lblFechaNacimiento.Text = "Fecha nacimiento:";
+            // 
             // AgregarEmpleado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 194);
+            this.ClientSize = new System.Drawing.Size(505, 382);
+            this.Controls.Add(this.lblFechaNacimiento);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.lblSignoPesos);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.chkEsAdmin);
+            this.Controls.Add(this.lblClave);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblNombreUsuario);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblDni);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.txtApellidoEmpleado);
+            this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.btnConfirmar);
             this.Controls.Add(this.txtNombreEmpleado);
             this.Name = "AgregarEmpleado";
@@ -68,5 +223,20 @@
 
         private System.Windows.Forms.TextBox txtNombreEmpleado;
         private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtApellidoEmpleado;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label lblNombreUsuario;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox chkEsAdmin;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblSignoPesos;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label lblFechaNacimiento;
     }
 }
